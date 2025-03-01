24747339e3d34b4e3a5a27cde7d9cd50d9c21fb3
vanous <petr@linuks.cz>
2025-03-01 22:02:28 +0100

    Move svelte to a frontend directory

--- frontend/compile_svelte.sh
+++ frontend/compile_svelte.sh
@@ -17,6 +17,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 npm run build
-rm -r ./backend/static/*
-cp -r ./dist/*  ./backend/static/
+rm -r ../backend/static/*
+cp -r ./dist/*  ../backend/static/
 
